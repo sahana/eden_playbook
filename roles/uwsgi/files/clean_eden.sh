d7aaccb50d1937bb058b1b054d07d3b2d7e5a6ed
Gaurav Narula <gnarula94@gmail.com>
2014-08-08 17:57:17 +0530

    Split clean script

--- roles/uwsgi/files/clean_eden.sh
+++ roles/uwsgi/files/clean_eden.sh
@@ -7,20 +7,7 @@ elif [[ ! -d "/home/$1" ]]; then
     echo >&2 "$1 is not a valid instance!"
     exit 1
 fi
-INSTANCE=$1
-if [[ "$1" = "prod" ]]; then
-    echo "You selected: Production"
-    echo -n "Are you absolutely sure? (yes/n):"
-    read confirm
-    if [ "$confirm" != "yes" ]; then
-        echo "Cancelled"; exit
-    fi
-    DATABASE="sahana"
-else
-    DATABASE="sahana-$INSTANCE"
-fi
-echo >&2 "Cleaning instance: $INSTANCE"
-/etc/init.d/uwsgi-$INSTANCE stop
+#/etc/init.d/uwsgi-$INSTANCE stop
 cd /home/$INSTANCE/applications/eden
 rm -rf databases/*
 rm -f errors/*
@@ -28,24 +15,11 @@ rm -f sessions/*
 rm -rf uploads/*
 echo >&2 "Dropping database: $DATABASE"
 set +e
-pkill -f "postgres: sahana $DATABASE"
-sudo -H -u postgres dropdb $DATABASE
-set -e
-echo >&2 "Creating database: $DATABASE"
-su -c - postgres "createdb -O sahana -E UTF8 -l en_US.UTF-8 $DATABASE -T template0"
 if [[ "$1" = "test" ]]; then
-    echo >&2 "Refreshing database from Production: $DATABASE"
-    su -c - postgres "pg_dump -c sahana > /tmp/sahana.sql"
-    su -c - postgres "psql -f /tmp/sahana.sql $DATABASE"
     cp -pr /home/prod/applications/eden/databases/* /home/$INSTANCE/applications/eden/databases/
     cd /home/$INSTANCE/applications/eden/databases
     for i in *.table; do mv "$i" "${i/PROD_TABLE_STRING/TEST_TABLE_STRING}"; done
 else
-    echo >&2 "Migrating/Populating database: $DATABASE"
-    #su -c - postgres "createlang plpgsql -d $DATABASE"
-    su -c - postgres "psql -q -d $DATABASE -f /usr/share/postgresql/9.3/extension/{{ postgis_version.stdout }}"
-    su -c - postgres "psql -q -d $DATABASE -c 'grant all on geometry_columns to sahana;'"
-    su -c - postgres "psql -q -d $DATABASE -c 'grant all on spatial_ref_sys to sahana;'"
     echo >&2 "Starting DB actions with eden"
     cd /home/$INSTANCE/applications/eden
     sed -i 's/settings.base.migrate = False/settings.base.migrate = True/g' models/000_config.py
@@ -60,10 +34,4 @@ fi
 echo >&2 "Compiling..."
 cd /home/$INSTANCE
 python web2py.py -S eden -M -R applications/eden/static/scripts/tools/compile.py
-/etc/init.d/uwsgi-$INSTANCE start
-if [[ "$1" = "test" ]]; then
-   echo >&2 "pass"
-else
-   cd /home/$INSTANCE
-   #sudo -H -u web2py python web2py.py -S eden -M -R /home/data/import.py
-fi
+#/etc/init.d/uwsgi-$INSTANCE start
